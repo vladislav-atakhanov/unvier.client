085aed89b822804883b97c69057f66e3e1c04d67
Vladislav Atakhanov <eloh1mZee@gmail.com>
2023-12-14 20:51:26 +0600

    feat: trotling for requests

--- src/api/data.ts
+++ src/api/data.ts
@@ -1,7 +1,7 @@
 import { authFetch } from "./auth"
 import { api } from "./config"
 
-import { type Readable, writable } from "svelte/store"
+import { type Readable, writable, readable } from "svelte/store"
 import { onMount } from "svelte"
 
 export const createFetchData =
@@ -9,30 +9,41 @@ export const createFetchData =
     () =>
         authFetch<T>(api(path))
 
+const lastUpdates = new Map<string, number>()
+
+type Store<T> = Readable<T | null> & { update: () => void }
+
+const UPDATE_DELAY = 5 * 60 * 1000
 export const createUseData =
     <T>(fetchData: () => Promise<T>, key: string, check = (d: T) => !!d) =>
-    (): [Readable<T | null> & { refresh: () => void }, Readable<boolean>] => {
+    (): [Store<T>, Readable<boolean>] => {
         const { subscribe, set } = writable<T | null>(null)
-        const loading = writable(true)
+        const loading = writable(false)
 
-        const refresh = async () => {
+        const update = async () => {
             loading.set(true)
+            console.log("fetch")
             const data = await fetchData()
+            console.log("fetch ok")
+            loading.set(false)
+            lastUpdates.set(key, Date.now())
             if (data && check(data)) {
                 set(data)
                 localStorage.setItem(key, JSON.stringify(data))
             }
-            loading.set(false)
+        }
+
+        const needUpdate = (callback: () => void) => {
+            const now = Date.now()
+            const lastUpdate = lastUpdates.get(key) || 0
+            if (now - lastUpdate >= UPDATE_DELAY) callback()
         }
 
         onMount(() => {
             const data = localStorage.getItem(key)
             if (data) set(JSON.parse(data))
-            refresh()
-            return () => {
-                set(null)
-                loading.set(true)
-            }
+            needUpdate(update)
+            return () => set(null)
         })
-        return [{ subscribe, refresh }, { subscribe: loading.subscribe }]
+        return [{ subscribe, update }, { subscribe: loading.subscribe }]
     }
