3893746ea3148b5157047f392b64656bf2311ba0
Vladislav Atakhanov <eloh1mZee@gmail.com>
2024-01-17 18:00:18 +0600

    fix: api-host

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,2 +1,4 @@
-const API_PATH = import.meta.env.DEV ? "http://localhost:7435" : ""
+const API_PATH = import.meta.env.DEV
+    ? "http://localhost:7435"
+    : "https://univerapi.eloh1m.com"
 export const api = (path: string) => `${API_PATH}${path}`
