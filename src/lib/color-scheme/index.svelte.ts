b952611802e7bbdce232d93bb173daa198a307bf
Vladislav Atakhanov <eloh1mzee@gmail.com>
2024-10-23 22:08:39 +0500

    feat: save ColorScheme to LocalStorage

--- src/lib/color-scheme/index.svelte.ts
+++ src/lib/color-scheme/index.svelte.ts
@@ -1,8 +1,26 @@
-export class ColorScheme {
-    value = $state<"auto" | "dark" | "light">("auto")
-    apply(node: HTMLElement) {
-        console.log(node)
+class StoredValue<T extends string> {
+    value = $state<T>("" as T)
+    constructor(public key: string, public defaultValue: T) {
+        this.value = defaultValue
+    }
+    apply(...args: unknown[]) {
+        $effect(() => {
+            if (this.value === this.defaultValue) {
+                localStorage.removeItem(this.key)
+            } else {
+                localStorage.setItem(this.key, this.value)
+            }
+        })
+        this.value = (localStorage.getItem(this.key) as T) ?? this.defaultValue
+    }
+}
 
+export class ColorScheme extends StoredValue<"auto" | "dark" | "light"> {
+    constructor() {
+        super("color-theme", "auto")
+    }
+    apply(node: HTMLElement) {
+        super.apply()
         $effect(() => {
             if (this.value === "auto") {
                 node.classList.remove("light", "dark")
