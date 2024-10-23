0f5fb4bce24d17557dce0ccf2fe3833c80ade2f8
Vladislav Atakhanov <eloh1mzee@gmail.com>
2024-10-23 19:55:17 +0500

    refactor: Checkbox

--- src/lib/components/ui/checkbox/checkbox.svelte
+++ src/lib/components/ui/checkbox/checkbox.svelte
@@ -1,35 +1,49 @@
 <script lang="ts">
-	import { Checkbox as CheckboxPrimitive } from "bits-ui";
-	import Check from "lucide-svelte/icons/check";
-	import Minus from "lucide-svelte/icons/minus";
-	import { cn } from "$lib/utils.js";
+    import Check from "lucide-svelte/icons/check"
+    import Minus from "lucide-svelte/icons/minus"
+    import type { HTMLInputAttributes } from "svelte/elements"
 
-	type $$Props = CheckboxPrimitive.Props;
-	type $$Events = CheckboxPrimitive.Events;
-
-	let className: $$Props["class"] = undefined;
-	export let checked: $$Props["checked"] = false;
-	export { className as class };
+    let {
+        class: class_,
+        checked = $bindable(false),
+        ...props
+    }: HTMLInputAttributes & { class?: string; checked?: boolean } = $props()
 </script>
 
-<CheckboxPrimitive.Root
-	class={cn(
-		"border-primary ring-offset-background focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground peer box-content h-4 w-4 shrink-0 rounded-sm border focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-50",
-		className
-	)}
-	bind:checked
-	{...$$restProps}
-	on:click
+<input type="checkbox" class="sr-only" bind:checked {...props} />
+<span
+    class="{class_} body border-primary ring-offset-background peer box-content h-4 w-4 shrink-0 rounded-sm border"
 >
-	<CheckboxPrimitive.Indicator
-		class={cn("flex h-4 w-4 items-center justify-center text-current")}
-		let:isChecked
-		let:isIndeterminate
-	>
-		{#if isChecked}
-			<Check class="h-3.5 w-3.5" />
-		{:else if isIndeterminate}
-			<Minus class="h-3.5 w-3.5" />
-		{/if}
-	</CheckboxPrimitive.Indicator>
-</CheckboxPrimitive.Root>
+    <span class="icon flex h-4 w-4 items-center justify-center text-current">
+        <Check class="check h-3.5 w-3.5" />
+        <Minus class="minus h-3.5 w-3.5" />
+    </span>
+</span>
+
+<style>
+    input:focus-visible + .body {
+        @apply ring-ring outline-none ring-2 ring-offset-2;
+    }
+    input:disabled + .body {
+        @apply cursor-not-allowed opacity-50;
+    }
+
+    input:focus-visible + .body {
+        @apply ring-ring outline-none ring-2 ring-offset-2;
+    }
+
+    input:checked + .body {
+        @apply bg-primary text-primary-foreground;
+    }
+
+    .body :global(.check),
+    .body :global(.minus) {
+        display: none;
+    }
+    input:indeterminate + .body :global(.minus) {
+        display: block;
+    }
+    input:checked + .body :global(.check) {
+        display: block;
+    }
+</style>
