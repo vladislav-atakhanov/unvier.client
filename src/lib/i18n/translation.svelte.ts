7b57fa5d8d8eb8bcf58af85d84e686a34b885f02
Vladislav Atakhanov <eloh1mzee@gmail.com>
2024-10-24 05:57:51 +0500

    fix: Translation types

--- src/lib/i18n/translation.svelte.ts
+++ src/lib/i18n/translation.svelte.ts
@@ -8,11 +8,10 @@ type Locale = (typeof locales)[keyof typeof locales]
 class Translation {
     language: keyof typeof locales = $state("ru")
     obj: Locale = $derived(locales[this.language])
-    #mount = false
     constructor(public translations: Record<string, Locale>) {}
-    translate(key: keyof Locale, ...params: string[]) {
+    translate(key: keyof Locale, ...params: unknown[]) {
         let result = this.translations[this.language][key]
-        for (const param of params) result = result.replace("{}", param)
+        for (const param of params) result = result.replace("{}", `${param}`)
         return result
     }
     apply(_: unknown) {
@@ -20,11 +19,10 @@ class Translation {
         $effect(() => {
             localStorage.setItem("lang", this.language)
             document.documentElement.setAttribute("lang", this.language)
-            this.#mount = true
         })
     }
 }
 export const i18n = new Translation(locales as any)
 
-export const _ = (key: keyof Locale, ...params: string[]) =>
+export const _ = (key: keyof Locale, ...params: unknown[]) =>
     i18n.translate(key, ...params)
