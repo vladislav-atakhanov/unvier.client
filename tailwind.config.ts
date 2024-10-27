4d92691e5e58fd72657ff73a19af31a7ccc170d0
Vladislav Atakhanov <eloh1mzee@gmail.com>
2024-10-27 17:13:25 +0500

    fix: disable hover on mobile devices

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,9 @@ const config: Config = {
     darkMode: ["class"],
     content: ["./src/**/*.{html,js,svelte,ts}"],
     safelist: ["dark"],
+    future: {
+        hoverOnlyWhenSupported: true,
+    },
     theme: {
         container: {
             center: true,
