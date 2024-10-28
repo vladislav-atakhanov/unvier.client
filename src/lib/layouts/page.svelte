51b81c965c984e64baec5079d4daec1d06a38974
Vladislav Atakhanov <eloh1mzee@gmail.com>
2024-10-29 04:05:54 +0500

    fix: add data-vaul-drawer-wrapper to page

--- src/lib/layouts/page.svelte
+++ src/lib/layouts/page.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
     import { useApp } from "../../app.svelte"
-    import { type Snippet } from "svelte"
+    import type { Snippet } from "svelte"
+    import type { SvelteHTMLElements } from "svelte/elements"
 
     let {
         class: class_ = "",
         children,
         header,
+        ...props
     }: {
         children?: Snippet
         class?: string
         header?: Snippet
         footer?: Snippet
-    } = $props()
+    } & SvelteHTMLElements["div"] = $props()
 
     const app = useApp()
 </script>
@@ -19,6 +21,8 @@
 <div
     class="page h-screen overflow-y-auto {class_} w-screen overflow-x-hidden"
     style:padding-bottom="{app.navigationHeight ?? 0}px"
+    data-vaul-drawer-wrapper
+    {...props}
 >
     {#if header}
         <div class="sticky left-0 right-0 top-0">{@render header()}</div>
