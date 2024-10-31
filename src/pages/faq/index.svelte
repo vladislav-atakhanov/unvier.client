707026c32dc854bd0b15bc8ba9a39267638b2d3b
Vladislav Atakhanov <eloh1mzee@gmail.com>
2024-10-31 14:55:57 +0500

    feat: add skeleton to FAQ

--- src/pages/faq/index.svelte
+++ src/pages/faq/index.svelte
@@ -1,11 +1,11 @@
 <script>
     import { useApi } from "$api"
     import AppBar from "$lib/components/app-bar.svelte"
-    import Loader from "$lib/components/loader.svelte"
     import Card from "$lib/components/ui/card"
+    import { Skeleton } from "$lib/components/ui/skeleton"
     import { _ }from "$lib/i18n"
     import Page from "$lib/layouts/page.svelte"
-    import { nullish } from "$lib/utils"
+    import { nullish, randInt } from "$lib/utils"
     import { routes } from "../url"
 
     const api = useApi()
@@ -18,8 +18,14 @@
     {/snippet}
 
     <div class="grid mx-auto p-2 gap-2 max-w-md">
-        {#if query.loading }
-            <Loader />
+        {#if query.loading}
+            {#each {length: 8} as _}
+                <Card>
+                    {#snippet title()}
+                        <Skeleton symbols={randInt(20, 50)} />
+                    {/snippet}
+                </Card>
+            {/each}
         {:else if !nullish(query.data)}
             {#each query.data as {id, label} (id)}
                 <Card href="{routes.faq}/{id}" title={label} />
