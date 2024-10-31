1ec0b2dcbfef17359de2ecc1e99a76d71ade421a
Vladislav Atakhanov <eloh1mzee@gmail.com>
2024-10-31 19:51:58 +0500

    feat: add polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
 import { VitePWA } from "vite-plugin-pwa"
+import legacy from "@vitejs/plugin-legacy"
+
 import path from "node:path"
 import manifest from "./src/manifest.js"
 
@@ -9,6 +11,9 @@ export default defineConfig({
     plugins: [
         svelte(),
         VitePWA({ manifest, includeAssets: ["images/icons.svg"] }),
+        legacy({
+            targets: ["defaults", "not IE 11"],
+        }),
     ],
     resolve: {
         alias: {
